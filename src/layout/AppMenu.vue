2d5e4525c327216a73b69d438328eef379737d1e
HEVERTON LUIZ MORAIS DE SOUZA <heversouz@gmail.com>
2024-01-05 15:24:39 -0300

    ultimas alterações app menu

--- src/layout/AppMenu.vue
+++ src/layout/AppMenu.vue
@@ -12,42 +12,97 @@ const model = ref([
         label: 'Hierarchy',
         items: [
             {
-                label: 'Submenu 1',
-                icon: 'pi pi-fw pi-bookmark',
+                label: 'RELATÓRIOS',
+                icon: 'pi pi-chart-bar',
                 items: [
-                    {
-                        label: 'Submenu 1.1',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [
-                            { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
-                            { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
-                            { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' }
-                        ]
-                    },
-                    {
-                        label: 'Submenu 1.2',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [{ label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }]
-                    }
+                    {label: 'FINANCEIRO',icon: 'pi pi-fw pi-bookmark'},
+                    {label: 'RESUMO FINANCEIRO',icon: 'pi pi-fw pi-bookmark'},
+                    {label: 'PERFORMANCE DO NEGÓCIO',icon: 'pi pi-fw pi-bookmark'},
+                    {label: 'DESPESAS DETALHADAS',icon: 'pi pi-fw pi-bookmark'},
                 ]
             },
             {
-                label: 'Submenu 2',
-                icon: 'pi pi-fw pi-bookmark',
+                label: 'ORÇAMENTOS',
+                icon: 'pi pi-fw pi-chart-line',
                 items: [
-                    {
-                        label: 'Submenu 2.1',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [
-                            { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
-                            { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' }
-                        ]
-                    },
-                    {
-                        label: 'Submenu 2.2',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [{ label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' }]
-                    }
+
+                    {label: 'CUSTOS',icon: 'pi pi-fw pi-bookmark'},
+                    {label: 'RESUMO FINANCEIRO',icon: 'pi pi-fw pi-bookmark'},
+                    {label: 'VENDAS',icon: 'pi pi-fw pi-bookmark'},
+                ]
+            },
+            {
+                label: 'CUSTOS',
+                icon: 'pi pi-fw pi-window-minimize',
+                items: [
+
+                    {label: 'NOVO CUSTO',icon: 'pi pi-fw pi-bookmark'},
+                    {label: 'SUMÁRIO DE CUSTOS',icon: 'pi pi-fw pi-bookmark'},
+                ]
+            },  
+            {
+                label: 'PRODUTOS',
+                icon: 'pi pi-fw pi-box',
+                items: [
+
+                    {label: 'NOVO PRODUTO',icon: 'pi pi-fw pi-bookmark'},
+                    {label: 'SUMÁRIO DE PRODUTOS',icon: 'pi pi-fw pi-bookmark'},
+                ]
+            },
+            {
+                label: 'COMPRAS',
+                icon: 'pi pi-fw pi-briefcase',
+                items: [
+
+                    {label: 'NOVA COMPRA',icon: 'pi pi-fw pi-bookmark'},
+                    {label: 'SUMÁRIO DE COMPRAS',icon: 'pi pi-fw pi-bookmark'},
+                ]
+            },
+            {
+                label: 'FORNECEDORES',
+                icon: 'pi pi-fw pi-building',
+                items: [
+
+                    {label: 'NOVO FORNECEDOR',icon: 'pi pi-fw pi-bookmark'},
+                    {label: 'LISTA DE FORNECEDORES',icon: 'pi pi-fw pi-bookmark'},
+                ]
+            },
+            {
+                label: 'VENDAS',
+                icon: 'pi pi-fw pi-dollar',
+                items: [
+
+                    {label: 'NOVA VENDA',icon: 'pi pi-fw pi-bookmark'},
+                    {label: 'SUMÁRIO DE VENDAS',icon: 'pi pi-fw pi-bookmark'},
+                ]
+            },
+            {
+                label: 'CLIENTES',
+                icon: 'pi pi-fw pi-users',
+                items: [
+
+                    {label: 'NOVO CLIENTE',icon: 'pi pi-fw pi-bookmark'},
+                    {label: 'LISTA DE CLIENTES',icon: 'pi pi-fw pi-bookmark'},
+                ]
+            },
+            {
+                label: 'COLABORADORES',
+                icon: 'pi pi-fw pi-users',
+                items: [
+
+                    {label: ' NOVO COLABORADOR',icon: 'pi pi-fw pi-bookmark'},
+                    {label: 'LISTA DE COLABORADORES',icon: 'pi pi-fw pi-bookmark'},
+                ]
+            },
+            {
+                label: 'MINHA CONTA',
+                icon: 'pi pi-fw pi-user',
+                items: [
+
+                    {label: 'SOBRE VOCÊ',icon: 'pi pi-fw pi-bookmark'},
+                    {label: 'SUA EMPRESA',icon: 'pi pi-fw pi-bookmark'},
+                    {label: 'SUA ASSINATURA',icon: 'pi pi-fw pi-bookmark'},
+                    {label: 'SUPORTE TÉCNICO',icon: 'pi pi-fw pi-bookmark'},
                 ]
             }
         ]
